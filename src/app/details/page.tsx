ef8dc3019998eb5d0aec222d0a32ecd7561feede
Mauriksk <otarksk@gmail.com>
2024-11-20 20:50:36 +0100

    consuming service

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -2,24 +2,32 @@
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+interface DetailsType {
+    imagesData: string[]
+}
+
 const DetailsPage = () => {
     const searchParams = useSearchParams();
     const id = searchParams.get('id');
-    const [details, setDetails] = useState()
-    const getDetails = async (id: string) => {
-        await fetch(`http://localhost:3000/api/details?id=${id}`)
+    const [details, setDetails] = useState<DetailsType>()
+    const getDetails = async (id: string): Promise<DetailsType> => {
+        const detailsData: DetailsType = await fetch(`http://localhost:3000/api/details?id=${id}`)
             .then(data => data.json())
-            .then(data => console.log(data))
+            .then(data => data)
             .catch(err => console.log(err))
+
+        return detailsData
     }
 
     useEffect(() => {
         (async () => {
-            getDetails(id || "")
+            const detailsData: DetailsType = await getDetails(id || "")
+            console.log("aca esta la data ", detailsData)
+            setDetails(detailsData)
         })()
     }, [])
     return (
-        <div>page {id}</div>
+       <div>hola papa</div>
     )
 }
 
